321254cbe854ea0fc46d9c3ebbac8621ba9b4b87
Ghendel <jian.bakhman@gmail.com>
2024-03-20 22:14:08 +0200

    mod_12 added pydentic schema for User

--- mod_12/src/schemas.py
+++ mod_12/src/schemas.py
@@ -1,6 +1,7 @@
+from datetime import date, datetime
 from typing import Optional
+
 from pydantic import BaseModel, EmailStr, Field
-from datetime import date
 
 
 class ContactModel(BaseModel):
@@ -20,3 +21,31 @@ class ResponseContactModel(BaseModel):
     phone: str
     birthday: date
     note: Optional[str]
+
+
+class UserModel(BaseModel):
+    username: str = Field(min_length=5, max_length=16)
+    email: EmailStr
+    password: str = Field(min_length=6, max_length=10)
+
+
+class UserDb(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    created_at: datetime
+    avatar: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserResponse(BaseModel):
+    user: UserDb
+    detail: str = "User successfully created"
+
+
+class TokenModel(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
