15240804719dd4f5d6e4fcebc6efbff870b15f0e
Ghendel <jian.bakhman@gmail.com>
2024-03-21 00:06:03 +0200

    mod_12 added start uvicorn to main.py

--- mod_12/main.py
+++ mod_12/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from src.routes import contacts
@@ -10,3 +11,7 @@ app.include_router(contacts.router, prefix="/api")
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
