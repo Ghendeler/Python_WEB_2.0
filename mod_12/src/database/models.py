497b0692c2260ca9d2d496402ca725cf9ed3d135
Ghendel <jian.bakhman@gmail.com>
2024-03-20 15:46:16 +0200

    mod_12 added Users table and migration

--- mod_12/src/database/models.py
+++ mod_12/src/database/models.py
@@ -1,5 +1,8 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Date, Integer, String, func
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import DateTime
 
 Base = declarative_base()
 
@@ -13,3 +16,17 @@ class Contact(Base):
     phone = Column(String(20))
     birthday = Column(Date)
     note = Column(String(250))
+    user_id = Column(
+        "user_id", ForeignKey("users.id", ondelete="CASCADE"), default=None
+    )
+    user = relationship("User", backref="notes")
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column("crated_at", DateTime, default=func.now())
+    refresh_token = Column(String(255), nullable=True)
